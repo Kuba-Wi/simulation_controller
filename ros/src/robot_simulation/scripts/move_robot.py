74378d72251a73d98fdeca03428d63839cdc48c7
kuba <winczukjakub@gmail.com>
2022-07-28 13:02:18 +0200

    added sending linear acceleration and angular velocity of the robot to the controller

--- ros/src/robot_simulation/scripts/move_robot.py
+++ ros/src/robot_simulation/scripts/move_robot.py
@@ -2,40 +2,49 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Imu
 
 import bluetooth
+import struct
 
 
 def calculate_vel(speed, direction):
     eps = 300
+    MAX_VAL = 4000
+    MAX_SPEED = 1
+    MIN_SPEED = -1
     vel_speed = 0
-    if abs(speed - 2000) > eps:
-        vel_speed = (speed - 2000) / 2000
-        if vel_speed > 1:
-            vel_speed = 1
-        elif vel_speed < -1:
-            vel_speed = -1
+    if abs(speed - MAX_VAL / 2) > eps:
+        vel_speed = (speed - MAX_VAL / 2) / (MAX_VAL / 2)
+        if vel_speed > MAX_SPEED:
+            vel_speed = MAX_SPEED
+        elif vel_speed < MIN_SPEED:
+            vel_speed = MIN_SPEED
 
-    return vel_speed, 0 if abs(direction - 2048) < 300 else (2048 - direction) / 2048
+    if direction > MAX_VAL:
+        direction = MAX_VAL
+
+    return vel_speed, 0 if abs(direction - MAX_VAL / 2) < eps else (MAX_VAL / 2 - direction) / (MAX_VAL / 2)
 
 
-def talker():
-    pub = rospy.Publisher('robot_diffdrive_controller/cmd_vel', Twist, queue_size=10)
-    rospy.init_node('robot_move_node', anonymous=True)
-    # rate = rospy.Rate(100)
-
+def activate_nodes():
     bd_addr = "98:D3:C1:FD:C3:DF"
     port = 1
     sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     sock.connect((bd_addr, port))
 
+    rospy.init_node('robot_move_node', anonymous=True)
+    pub = rospy.Publisher('robot_diffdrive_controller/cmd_vel', Twist, queue_size=10)
+    sub = rospy.Subscriber('imu', Imu, list_callback, sock)
+
     move = Twist()
     buffer = bytearray()
+    BUF_LEN = 6
 
     while not rospy.is_shutdown():
         data = sock.recv(1024)
         buffer.extend(data)
-        if len(buffer) >= 6:
+        if len(buffer) >= BUF_LEN:
             speed = int.from_bytes(buffer[0:4], "little")
             direction = int.from_bytes(buffer[4:], "little")
             print("received: (speed, rotation)", speed, direction)
@@ -47,11 +56,14 @@ def talker():
 
             buffer.clear()
 
-        # rate.sleep()
+
+def list_callback(data, sock):
+    byt = struct.pack('dd', data.linear_acceleration.x, data.angular_velocity.z)
+    sock.send(byt)
 
 
 if __name__ == '__main__':
     try:
-        talker()
+        activate_nodes()
     except rospy.ROSInterruptException:
         pass
